0b44b7c34ddfa19ff78077c933805c5a314e0289
santushtigandhi <aman.goyal16sms@gmail.com>
2023-10-26 16:20:29 +0530

    Corey's Lecture 2

--- Python/Strings.py
+++ Python/Strings.py
@@ -21,3 +21,43 @@ print(long_message)
 # String functions
 message = 'Hello World'
 print(len(message))     # Length of the string
+
+# Accessing characters of the string
+print(message[6])       # Printing W
+
+# Accessing only Hello
+print(message[0:5])     # 0 is inclusive and 5 is exclusive. 
+print(message[:5])
+print(message[6:])
+
+print(message.lower())  # Lower case everything
+print(message.upper())  # Upper case everytyhing
+
+# Counting a certain number of word/alphabet
+print(message.count('Hello'))
+print(message.count('l'))
+
+# Finding a certain alphabet/word
+print(message.find('World'))    # Answer is 6 because World starts at 6
+print(message.find('l'))
+print(message.find('z'))        # Gives -1 as it does not exist
+
+# Replacing
+new_message = message.replace('World', 'Universe')
+print(new_message)
+
+# Adding/Concatenating strings
+greeting = 'Hello'
+name = 'Aman'
+# message = greeting + ' ' + name + '. Welcome!'
+# print(message)
+
+# + is useful for small sentences. But for longer sentences, we will use another method.
+# We will use formatted strings
+message = f'{greeting}, {name}. Welcome!'
+print(message)
+
+# Getting all the attributes, methods and functions we can use
+print(dir(name))
+
+print(help(str))        #help(str.lower) will give information about lower function
