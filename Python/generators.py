0fefc3ce6328334f55a258f75644e50b8c882784
santushtigandhi <aman.goyal16sms@gmail.com>
2023-11-09 19:27:07 +0530

    Corey's Lecture 16

--- Python/generators.py
+++ Python/generators.py
@@ -15,6 +15,17 @@ def square_nums(nums):
 
 my_nums = square_nums([1, 2, 3, 4, 5])
 
-
+# A generator function holds only one value at a time which is why we use a for loop or use -
+# print(next(num))
 for num in my_nums:
     print(num)
+
+# Above code using list comprehension
+my_nums1 = (x*x for x in [1,2,3,4,5])
+
+# for num in my_nums1:
+#     print(num)
+
+# Printing the result by converting result from a generator function to a list
+my_nums1 = (x*x for x in [1,2,3,4,5])
+print(list(my_nums1))
