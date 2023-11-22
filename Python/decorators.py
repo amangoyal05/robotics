633c33d9d32cc1f1032f1365ae1d6d979c31ecec
santushtigandhi <aman.goyal16sms@gmail.com>
2023-11-22 15:19:53 +0530

    Corey's Lecture 25

--- Python/decorators.py
+++ Python/decorators.py
@@ -1,8 +1,29 @@
-def outer_function():
-    message = 'Hi'
+# Decorator - Function that takes another function as an argument, adds some functionality and returns another function without altering the source code of the original function that we passed
 
-    def inner_function():
-        print(message)
-    return inner_function()
+def decorator_func(original_function):
+    def wrapper_func(*args, **kwargs):
+        print(f'Wrapper executed this before {original_function.__name__}')
+        return original_function(*args, **kwargs)
+    return wrapper_func
 
-outer_function()
+
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print(f'Call Method executed this before {self.original_function.__name__}')
+        return self.original_function(*args, **kwargs)
+    
+# @decorator_func
+@decorator_class
+def display():
+    print('Display Function ran')
+
+# @decorator_func
+@decorator_class
+def display_info(name, age):
+    print(f'display_info ran with arguments ({name}, {age})')
+
+display()
+display_info('John', 25)
