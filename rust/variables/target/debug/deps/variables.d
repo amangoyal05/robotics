2cca5f972bacf71f40a6e65444176db5249972b9
amangoyal05 <aman.goyal16sms@gmail.com>
2024-02-10 22:39:10 +0530

    Mutable Variables

---
+++ rust/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+D:\Coding\robotics\rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+D:\Coding\robotics\rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ rust/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+D:\Coding\robotics\rust\variables\target\debug\variables.exe: D:\Coding\robotics\rust\variables\src\main.rs
